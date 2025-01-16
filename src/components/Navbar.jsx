3e5eb07d78baa2a8fcdc263cb1b02b7ddeae9c7b
SI-tanbir <si017490@gmail.com>
2025-01-16 19:36:30 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+"use client"
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+const Navbar = () => {
+  const links = [
+    {
+      title: 'home',
+      path: '/',
+    },
+    {
+      title: 'service',
+      path: '/service',
+    },
+    {
+      title: 'blog',
+      path: '/blog',
+    },
+  ];
+  const pathname=usePathname()
+
+  return (
+    <div className="bg-red-300">
+      <ul className="flex gap-10 text-2xl text-center justify-center">
+        {links?.map((link) =>  (
+
+
+       
+
+
+        
+          // Explicitly return the JSX element inside map
+          <li key={link.path}>
+            <Link className={`${pathname === link.path && "text-cyan-300"}`} href={link.path}>{link.title}</Link>
+          </li>
+  ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;

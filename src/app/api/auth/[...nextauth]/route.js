706d2bda8c5da9e846775abecf2bf13ac13fcaea
SI-tanbir <si017490@gmail.com>
2025-01-21 18:50:48 +0600

    experimenting it

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -14,7 +14,7 @@ const handler =NextAuth({
 
         CredentialsProvider({
             // The name to display on the sign in form (e.g. 'Sign in with...')
-            name: 'Credentials',
+            // name: 'Credentials',
             // The credentials is used to generate a suitable form on the sign in page.
             // You can specify whatever fields you are expecting to be submitted.
             // e.g. domain, username, password, 2FA token, etc.
